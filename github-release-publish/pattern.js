e18a150cb030d4323647a3ec79c756b7f8866df9
Andrew Spratley <aps831@yahoo.co.uk>
2024-03-18 18:39:02 +0000

    feat: accept multiple artifacts in github release publish

---
+++ github-release-publish/pattern.js
@@ -0,0 +1,28 @@
+let strings = process.argv[2].split("\n");
+
+String.prototype.strstr = function (search) {
+  var position = this.indexOf(search);
+  if (position == -1) {
+    return "";
+  }
+  return this.slice(position);
+};
+
+function downloadPattern(strings) {
+  const nonEmptyStrings = strings.filter((s) => s.length > 0);
+  if (nonEmptyStrings.length == 0) {
+    return "";
+  }
+  let prefix = nonEmptyStrings[0];
+  for (let i = 1; i < nonEmptyStrings.length; i++) {
+    while (nonEmptyStrings[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (!prefix) {
+        return "";
+      }
+    }
+  }
+  return nonEmptyStrings.length < 2 ? prefix : prefix + "*";
+}
+
+console.log(downloadPattern(strings));
